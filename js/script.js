249c74406f33c5467735b3b3f055e5674d0d89a3
sradin96 <sradin96@gmail.com>
2021-09-19 18:48:09 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$('.header__menu-link').on('click', function(){
+	$('.header__navigation').toggleClass('active-menu');
+	$('.header__cta-block').toggleClass('active-menu');
+	$('.header__account').toggleClass('active-menu');
+	$('.header__menu-link').toggleClass('toggle-menu');
+	if($(".header__navigation").hasClass("active-menu")) {
+		$('body').addClass('fixed-position');
+	} else {
+		$('body').removeClass('fixed-position');
+	}
+});
+
+var lastScrollTop = 0;
+$(document).ready(function(){
+	$(window).scroll(function(){
+		let scroll = $(this).scrollTop();
+
+		if (scroll > lastScrollTop && $(window).width() > 1024) {		
+			$(".header").addClass('scrolled-header');
+			$(".header__logo-img").addClass('scrolled-logo');
+		}
+		else if (scroll == 0){
+			$(".header").removeClass('scrolled-header');	
+			$(".header__logo-img").removeClass('scrolled-logo');
+		}
+		lastScrollTop = scroll;	
+	})	
+})
+
+var height = 600;
+$(document).ready(function(){
+	$(window).scroll(function(){
+        let scroll = $(this).scrollTop();
+
+        if(scroll >= height -400) {
+            $('.education__box').addClass("active-box");          
+        } else {
+            $('.education__box').removeClass("active-box");  
+        }
+
+        if(scroll >= height) {
+            $('.work__box').addClass("active-box");          
+        } else {
+            $('.work__box').removeClass("active-box");  
+        }
+        if(scroll >= height + 250) {
+            $('.box--2').addClass("active-box");          
+        } else {
+            $('.box--2').removeClass("active-box");  
+        }
+        if(scroll >= height + 500) {
+            $('.box--3').addClass("active-box");          
+        } else {
+            $('.box--3').removeClass("active-box");  
+        }
+        if(scroll >= height + 750) {
+            $('.box--4').addClass("active-box");          
+        } else {
+            $('.box--4').removeClass("active-box");  
+        }
+        if(scroll >= height + 1000) {
+            $('.box--5').addClass("active-box");          
+        } else {
+            $('.box--5').removeClass("active-box");  
+        }
+        if(scroll >= height + 1200) {
+            $('.box--6').addClass("active-box");          
+        } else {
+            $('.box--6').removeClass("active-box");  
+        }
+    })	
+})
